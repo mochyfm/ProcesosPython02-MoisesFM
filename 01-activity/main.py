c551ae02fde11bb8ed18d7029a3cc68ffd2802ef
Moisés Fernández Muiña <mochyfm@gmail.com>
2022-11-14 13:17:05 +0000

    Refactor(01-activity/main.py): Refactorized the code for the repetetive steps, so its more representative

--- 01-activity/main.py
+++ 01-activity/main.py
@@ -3,32 +3,36 @@ import sys
 from multiprocessing import Process
 from time import sleep
 
-def padre(customNumber):
+def callProcessNumber(loopNumber):
 
-    if platform.system() == "Windows":
-        
-        print("WINDOWS (PADRE) -> PID:", os.getpid(), '\n');
+    platformOs = platform.system()
+    print(platformOs.capitalize() + " (PADRE) -> PID:", os.getpid(), '\n');
 
-        for idx in range(customNumber):
-            print("Llamando al", str(idx + 1) + "º proceso.")
+    for idx in range(loopNumber):
+        print("Llamando al", str(idx + 1) + "º proceso.")
+        if platformOs == "Windows":
             process = Process(target=hijo);
             process.start();
             process.join(0);
-
-    elif platform.system() == "Linux":
-
-        print("LINUX (PADRE) -> PID:", os.getpid());
-
-        for idx in range(customNumber):
-            print("Llamando al", (idx + 1), "º proceso.")
+        elif platformOs == "Linux":
             newPid = os.fork();
             if newPid == 0:
                 hijo();
+        else:
+            print('No se ejecutar procesos en tu OS.')
+            break;
+    
+def padre(customNumber):
+    callProcessNumber(customNumber);
+    print('\nYa he terminado de llamar.');
 
 def hijo():
-    print('\n > Hijo con PID:', os.getpid(), '\n');
+    
+    processId = str(os.getpid());
+
+    print('\n > Hijo con PID: ' + processId + '\n');
     sleep(5);
-    print('\nEl proceso con PID:', os.getpid(), "ha muerto.\n")
+    print('\nEl proceso con PID: ' + processId + " ha muerto.\n")
     sys.exit(0);
 
 if __name__ == "__main__":
